19f1256d087ac55b9ee2d6dbc110ff59530d3557
Yves-Developer <yvesmugisha09@gmail.com>
2025-05-01 21:58:41 +0200

    # Start server

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import httpServer from "./server";
-httpServer.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+// Start the server on a specific port
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- server.ts
+++ server.ts
@@ -47,10 +47,6 @@ io.on("connection", (socket) => {
   });
 });
 
-// Start the server on a specific port
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 
 export default httpServer;
