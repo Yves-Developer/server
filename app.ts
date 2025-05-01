f64eb218a66ac9ac9062ce9c7f227dc7b081361c
Yves-Developer <yvesmugisha09@gmail.com>
2025-05-01 21:04:22 +0200

    # Cors permision added

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 import e from "express";
 const app = express();
 app.use(express.json());
+app.use(cors({origin: "*",methods: ["GET", "POST", "PUT", "DELETE"]}));
 app.use("/api", router);
 export default app;
