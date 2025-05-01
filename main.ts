abba7a9df9c500bb3d90147baa4c78572a45bac4
Yves-Developer <yvesmugisha09@gmail.com>
2025-05-01 18:52:33 +0200

    # Ultimate deploy set up

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import httpServer from "server";
+import httpServer from "./server";
 httpServer.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import app from "app";
+import app from "./app";
 import { createServer } from "http";
 import { Server } from "socket.io";
 const httpServer = createServer(app);
